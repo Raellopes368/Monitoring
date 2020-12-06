d59aa74e5b72c8a5968b01d65a44fb5214d813ef
Raellopes368 <raellopes368@gmail.com>
2020-12-06 19:11:33 -0300

    Adiciona rotas de inserção e consultas

---
+++ src/app/controllers/LocationController.js
@@ -0,0 +1,40 @@
+const Location = require('../models/location');
+
+class LocationController {
+  async store(req, res) {
+    const { latitude, longitude } = req.body;
+
+    const location = await Location.create({
+      latitude,
+      longitude,
+    });
+    return res.json(location);
+  }
+
+  async index(req, res) {
+    const {
+      day = new Date().getDate(),
+      month = new Date().getMonth() + 1,
+      initMonth = new Date().getMonth() - 1,
+      year = new Date().getFullYear(),
+    } = req.query;
+
+    const locations = await Location.find({
+      $and: [
+        {
+          createdAt: {
+            $gte: new Date(year, initMonth, day),
+            $lte: new Date(year, month, day),
+          },
+        },
+      ],
+    });
+    return res.json(locations);
+  }
+
+  async show(req, res) {
+    const location = await Location.findOne({}).sort({ createdAt: -1 });
+    return res.json(location);
+  }
+}
+module.exports = new LocationController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const { Router } = require('express');
+const LocationController = require('./app/controllers/LocationController');
 
 const routes = Router();
 
 routes.get('/', (req, res) => res.json({ status: 'running' }));
+
+routes.get('/locations', LocationController.index);
+
+routes.get('/locations/last', LocationController.show);
+
+routes.post('/locations', LocationController.store);
 
 module.exports = routes;
